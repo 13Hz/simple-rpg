368254103c26104a64274dc4e8e63e45d2f934da
13Hz <async13hhz@gmail.com>
2024-02-15 17:59:54 +0300

    Тесты

--- src/tests/classes/gameObject.test.ts
+++ src/tests/classes/gameObject.test.ts
@@ -21,12 +21,26 @@ describe('GameObject class', () => {
             object.update();
             expect(object.isHover).toBe(false);
         });
-        test('get center function', () => {
+    });
+    describe('check in room function', () => {
+        test('out of room', () => {
+            const object = new GameObject({x: -50, y: -50});
+            object.checkInRoom();
+            expect(object.isAlive).toBe(false);
+        });
+        test('in room', () => {
+            const object = new GameObject({x: 50, y: 50});
+            object.checkInRoom();
+            expect(object.isAlive).toBe(true);
+        });
+    });
+    describe('check center functions', () => {
+        test('get center', () => {
             const object = new GameObject({x: 100, y: 150}, 20);
             const expectedCenter: Point = new Point(110, 160);
             expect(object.getCenter()).toStrictEqual(expectedCenter);
         });
-        test('set center function', () => {
+        test('set center', () => {
             const object = new GameObject({x: 100, y: 150});
             object.setCenter(Point.empty);
             expect(object.getCenter()).toStrictEqual(Point.empty);
