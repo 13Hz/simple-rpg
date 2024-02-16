4b597a87aa57d45cbb2fab36308713160e6f0597
13Hz <async13hhz@gmail.com>
2024-02-16 10:56:34 +0300

    Рефакторинг тестов

--- src/tests/classes/gameObject.test.ts
+++ src/tests/classes/gameObject.test.ts
@@ -6,6 +6,27 @@ import {GameObject} from "../../classes/gameObject";
 import {Point} from "../../classes/point";
 
 describe('GameObject class', () => {
+    describe('checkCollision function', () => {
+        test('two objects intersect', () => {
+            const object1 = new GameObject({x: 0, y: 0});
+            const object2 = new GameObject({x: 10, y: 0});
+            expect(object1.checkCollision(object2)).toBe(true);
+        });
+        test('two objects do not intersect (end to end)', () => {
+            const object1 = new GameObject({x: 0, y: 0});
+            const object2 = new GameObject({x: 10.1, y: 0});
+            expect(object1.checkCollision(object2)).toBe(false);
+        });
+        test('two objects do not intersect (far)', () => {
+            const object1 = new GameObject({x: 0, y: 0});
+            const object2 = new GameObject({x: 500, y: 500});
+            expect(object1.checkCollision( object2)).toBe(false);
+        });
+        test('an object intersects itself', () => {
+            const object = new GameObject({x: 0, y: 0});
+            expect(object.checkCollision(object)).toBe(true);
+        });
+    });
     describe('update method', () => {
         test('is mouse hover', () => {
             //default mouse position - 0, 0
@@ -20,18 +41,6 @@ describe('GameObject class', () => {
             object.point = {x: 50, y: 50};
             object.update();
             expect(object.isHover).toBe(false);
-        });
-    });
-    describe('check in room function', () => {
-        test('out of room', () => {
-            const object = new GameObject({x: -50, y: -50});
-            object.checkInRoom();
-            expect(object.isAlive).toBe(false);
-        });
-        test('in room', () => {
-            const object = new GameObject({x: 50, y: 50});
-            object.checkInRoom();
-            expect(object.isAlive).toBe(true);
         });
     });
     describe('check center functions', () => {
